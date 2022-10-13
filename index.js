804cd86eedca7be6051fdb90eace4270207f6c1c
Christopher Sloss <ttg1989_89@protonmail.com>
2022-10-13 17:52:10 -0400

    commit tree 2d8a2a8adfb3f05fec2eb12fca723d8917e5c379

--- index.js
+++ index.js
@@ -1,1 +1,5 @@
-// Write your code here!
+const element = document.getElementById("main");
+element.remove();
+const newHeader = document.createElement("h1");
+newHeader.id = "victory";
+newHeader.textContent = "Chris Sloss is the champion";
